d2856b7d914621182d099f7c9da33c39d062b0fb
safil badarudeen <safilbadarudeen@gmail.com>
2022-12-12 13:15:09 +0530

    removed unwanted data

--- src/context.js
+++ src/context.js
@@ -2,7 +2,7 @@ import React, { useState, useContext, useReducer, useEffect } from 'react'
 import cartItems from './data'
 import reducer from './reducer'
 
-const url = 'https://course-api.com/react-useReducer-cart-project'
+
 const AppContext = React.createContext()
 
 const initialState={
